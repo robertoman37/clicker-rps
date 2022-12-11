83267f7ed61b9798f28327d7863359a3ee1759d0
RobertHoeh <92748186+robertoman37@users.noreply.github.com>
2022-12-11 21:57:29 +0000

    Fixed all the bugs

--- main.py
+++ main.py
@@ -1,18 +1,69 @@
-import curses as curse
+import curses
 import sys
 import random as rand
 import numpy as np
-from menu_base import menu_base
 
+class menu_base:
+    """A base menu class for methods to make making menus in this game easier.
+    Contains basic methods which should be inside the main event loop for all
+    menus in the game. Note for future me: remember to make a main function
+    that takes the return parameters for all classes and directs them to the
+    next class, stores global variables, and calls curses.wrapper() inside it.
+    """
 
-class home_menu:
+    def __init__(self, menu_items, menu_pos):
+        """define in here whatever other things you need to do for your menu to
+        work."""
+        self.menu_items = menu_items
+        self.menu_pos = menu_pos
+        self.cursor_pos = [0, 0]
+
+    def Input(self, w):
+        uin = w.getch()
+        if uin == 259:
+            self.cursor_pos[0] -= 1
+
+        elif uin == 258:
+            self.cursor_pos[0] += 1
+
+        elif uin == 261:
+            self.cursor_pos[1] += 1
+
+        elif uin == 260:
+            self.cursor_pos[1] -= 1
+
+        elif uin == 10:
+            return None
+        #   ^^^ Will eventually assign values to each menu. Uinimplemented for
+        #       now
+
+    def write_buffer(self, w):
+        for p1, i in enumerate(self.menu_items):
+            for p2, v in enumerate(i):
+                if self.menu_items[self.cursor_pos[0] % len(self.menu_items)]\
+                        [self.cursor_pos[1] % len(i)] == v:
+                    w.addstr(self.menu_pos[p1][p2][0],
+                             self.menu_pos[p1][p2][1],
+                             v,
+                             curses.A_REVERSE)
+                else:
+                    w.addstr(self.menu_pos[p1][p1][0],
+                             self.menu_pos[p1][p2][1],
+                             v)
+        w.refresh()
+        
+
+class home_menu(menu_base):
 
     def __init__(self, last_move):
-        menu = menu_base()
-        menu.init((("rock", "paper", "scissors"),
-                        ("shop", "options", "exit")),
-                       (((11, 5), (11, 17), (11, 28)),
-                        ((13, 5), (13, 16), (13, 30))))
+        """last move is the winner of the last turn.
+        0 = no one
+        1 = player
+        2 = ai
+        -1 = custom message, reserved value"""
+        menu_items = (("rock", "paper", "scissors"), ("shop", "options", "exit"))
+        menu_pos = (((11, 5), (11, 17), (11, 28)), ((13, 5), (13, 16), (13, 30)))
+        super().__init__(menu_items, menu_pos)
         self.last_move = [1, 1]
         self.money = 0
         self.graphics_list = ["""\
@@ -50,9 +101,9 @@ class home_menu:
 
     def home(self, w):
         a = lambda:\
-            f"{' '*37}" if self.first_turn else\
-            f"{' '*13}Player Won!{' '*13}" if self.player_won == True else\
-            f"{' '*15}AI Won!{' '*15}" if self.player_won == False else\
+            f"{' '*37}" if self.last_move == -1 else\
+            f"{' '*13}Player Won!{' '*13}" if self.last_move == 1 else\
+            f"{' '*15}AI Won!{' '*15}" if self.last_move == 2 else\
             f"{' '*13}Nobody Won!{' '*13}"
         curses.curs_set(0)
         w.clear()
@@ -65,8 +116,7 @@ class home_menu:
                   "│    rock    │   paper   │  scissors  │\n"+
                   "├────────────┼───────────┼────────────┤\n"+
                   "│    shop    │  options  │    exit    │\n"+
-                  "└────────────┴───────────┴────────────┘\n"+
-                  f"{self.player_won}")
+                  "└────────────┴───────────┴────────────┘\n")
         if self.last_move[0] in range(len(self.graphics_list)) or\
         self.last_move[1] in range(len(self.graphics_list)):
             player_graphic = self.graphics_list[self.last_move[0]].splitlines()
@@ -79,10 +129,10 @@ class home_menu:
         while True:
             try:
                 # get input and change cursor_pos correspondingly
-                menu.Input()
+                super().Input(w)
 
                 # write buffer: includes graphical renditions.
-                menu_base.write_buffer(curses)
+                super().write_buffer(w)
             except IndexError:
                 while True:
                     w.clear()
@@ -101,5 +151,5 @@ class home_menu:
         else:
             self.player_won = None
 
-game = home_menu([1,1])
+game = home_menu(-1)
 game.main()
